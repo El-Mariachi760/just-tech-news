223ba5314f13d8ab1e37c3114297ee615baaca11
Pancho Villa <94568874+El-Mariachi760@users.noreply.github.com>
2022-07-27 16:14:17 -0700

    Created partials for comments and post info

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
